148d0edeebb0321ecbdec5a551b71a5b472e1f9a
shruti ghosh <vishwanath15@hotmail.com>
2024-03-06 02:01:00 +0530

    first commit

---
+++ allInOne/src/test/java/com/db/pract/allInOne/AllInOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.pract.allInOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllInOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
