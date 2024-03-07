29840308406aa6fdd7265c066a5098835c366865
shruti ghosh <vishwanath15@hotmail.com>
2024-03-08 03:17:41 +0530

    test1

--- src/main/java/com/db/aoapp/AoSpringBootApplication.java
+++ src/main/java/com/db/aoapp/AoSpringBootApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AoapplicationApplication {
+public class AoSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AoapplicationApplication.class, args);
+		SpringApplication.run(AoSpringBootApplication.class, args);
+		System.out.println("AoapplicationApplication started!");
 	}
 
 }
--- src/test/java/com/db/aoapp/AoSpringBootApplicationTests.java
+++ src/test/java/com/db/aoapp/AoSpringBootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AoapplicationApplicationTests {
+class AoSpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
